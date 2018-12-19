da938a212e88213b5bbec6b66df751812437b4db
Joseph Morag <jm4157@columbia.edu>
2018-12-19 23:45:34 +0200

    Implement cd to home, full history, and correct counting bug

--- src/main.rs
+++ src/main.rs
@@ -21,70 +21,105 @@ fn main() {
             .read_line(&mut line)
             .expect("Failed to read line");
 
-        history.add(line);
-        execute(&history);
+        if history.process(line) {
+            execute(&history);
+        }
     }
 }
 
 fn execute(history: &History) {
-    let tokens = history.tokenize();
-    match tokens[0] {
-        "" => return,
-        "exit" => process::exit(0),
-        "cd" => env::set_current_dir(Path::new(tokens[1])).unwrap_or_else(|e| println!("{}", e)),
+    if history.current().is_none() {
+        return;
+    }
 
-        "history" => history.display(None),
-        _ => match Command::new(tokens[0]).args(&tokens[1..]).output() {
+    let tokens: Vec<&str> =
+        history.current().unwrap().split_whitespace().collect();
+    match tokens.as_slice() {
+        [] => return,
+        ["exit"] => process::exit(0),
+        ["cd"] => env::set_current_dir(Path::new("/users/josephmorag/"))
+            .unwrap_or_else(|e| println!("{}", e)),
+        ["cd", path] => env::set_current_dir(Path::new(path))
+            .unwrap_or_else(|e| println!("{}", e)),
+
+        ["history"] => history.display(None),
+        ["history", n] => history.display(n.parse().ok()),
+        [cmd, args..] => match Command::new(cmd).args(args).output() {
             Err(e) => println!("{}", e),
             Ok(out) => print!("{}", String::from_utf8_lossy(&out.stdout)),
         },
     }
 }
 
-struct History(VecDeque<CommandEntry>);
+struct History {
+    buffer: VecDeque<String>,
+    count: usize,
+}
 
 impl History {
     fn new(max_capacity: usize) -> History {
-        History(VecDeque::with_capacity(max_capacity))
+        History {
+            buffer: VecDeque::with_capacity(max_capacity),
+            count: 0,
+        }
     }
 
     fn display(&self, nentries: Option<usize>) {
-        match nentries {
-            None => {
-                for entry in self.0.borrow() {
-                    println!("{}", entry);
-                }
-            }
-            Some(n) => {
-                for i in 0..n {
-                    match self.0.get(i) {
-                        Some(entry) => println!("{}", entry),
-                        None => break,
-                    }
-                }
+        let n = nentries.unwrap_or(self.buffer.len());
+        for i in 0..n {
+            match self.buffer.get(i) {
+                Some(entry) => println!("{:<4} {}", i + self.count - n, entry),
+                None => break,
             }
         }
     }
 
-    fn add(&mut self, command: String) {
-        let next_ix = self.0.back().map_or(0, |x| x.1 + 1);
-        let new_entry = CommandEntry(command, next_ix);
-        if self.0.len() == 100 {
-            self.0.pop_front();
+    /// Handles all possible mutation of the history buffer
+    /// Returns a bool that signals if it is ok to continue executing a command
+    fn process(&mut self, command: String) -> bool {
+        let tokens: Vec<&str> = command.split_whitespace().collect();
+
+        if self.buffer.len() == 100 {
+            self.buffer.pop_front();
         }
-        self.0.push_back(new_entry);
+
+        let next_entry: Result<String, String> = match tokens.as_slice() {
+            ["!!"] => self
+                .buffer
+                .back()
+                .ok_or("Most recent command does not exist".to_string())
+                .map(|s| s.to_string()),
+            [cmd] if cmd.starts_with("!") => {
+                let needle = cmd.trim_start_matches("!");
+                self.buffer
+                    .iter()
+                    .rev()
+                    .find(|haystack| haystack.contains(needle))
+                    .ok_or(format!("{}: event not found", needle))
+                    .map(|s| s.to_string())
+            }
+
+            toks => Ok(toks.join(" ")),
+        };
+
+        if tokens.as_slice() == ["history", "-c"] {
+            self.buffer.clear();
+        }
+
+        match next_entry {
+            Ok(c) => {
+                self.buffer.push_back(c);
+                self.count += 1;
+                true
+            }
+            Err(e) => {
+                println!("{}", e);
+                false
+            }
+        }
     }
 
-    fn tokenize(&self) -> Vec<&str> {
-        // safe because this won't be called until a command is added
-        let CommandEntry(last_command, _) = self.0.back().unwrap();
-        last_command.trim().split(" ").collect()
-    }
-}
-
-struct CommandEntry(String, usize);
-impl fmt::Display for CommandEntry {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:<4} {}", self.1, self.0)
+    fn current(&self) -> Option<&str> {
+        self.buffer.back().map(|cmd| cmd.as_ref())
     }
 }
